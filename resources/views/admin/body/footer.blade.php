18faccc9d784f11a1b4826655645f7e120ab6c72
ynskrca12 <ynskrca52@outlook.com>
2023-03-18 03:55:41 +0300

    Admin Panel

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,16 @@
+
+
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+               Yunus Kırca  © Upcube.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Yunus Kırca 
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
