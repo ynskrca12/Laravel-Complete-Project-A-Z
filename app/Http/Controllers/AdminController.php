a958b2f024cdd20a40bc47fe1326ac376ad60e25
ynskrca12 <ynskrca52@outlook.com>
2023-03-19 03:32:39 +0300

    logout option

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(AdminController::class)->group(function (){
+    Route::get('/admin/logout','destroy')->name('admin.logout');
+});
+
+
+
+
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('admin.index');
