57c4ee451bb6c4d448f7d0dc4e72bed476fc859c
ynskrca12 <ynskrca52@outlook.com>
2023-03-25 06:19:44 +0300

    Admin Profile & Image Update Part 2

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 
 <div class="page-content">
     <div class="cantainer-fluid">
@@ -9,7 +10,8 @@
                     <div class="card-body">
                         <h4 class="card-title">Edit Profile Page</h4>
 
-                        <form action="">
+                        <form method="POST" action="{{route('store.profile')}}" enctype="multipart/form-data">
+                            @csrf
                         <div class="row mb-3">
                             <label for="example-text-input" class="col-sm-2 col-form-label">Name</label>
                             <div class="col-sm-10">
@@ -38,7 +40,7 @@
                         <div class="row mb-3">
                             <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                             <div class="col-sm-10">
-                                <input name="profile_image" class="form-control" type="file" id="example-text-input"
+                                <input name="profile_image" class="form-control" type="file" id="image"
                                 value="{{$editData->username}}">
                             </div>
                         </div>
@@ -47,7 +49,10 @@
                         <div class="row mb-3">
                             <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                             <div class="col-sm-10">
-                                <img src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap" 
+                                <img id="showImage" src="{{(!empty($editData->profile_image)) ? 
+                                    url('upload/admin_images/'.$editData->profile_image)
+                                    :url('upload/no_image.jpg')}}" 
+                                     alt="Card image cap" 
                                 class="rounded avatar-lg">
                             </div>
                         </div>
@@ -64,6 +69,18 @@
     </div>
 </div>
 
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#image').change(function (e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+
+</script>
 
 
 
--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -7,7 +7,11 @@
             <div class="col-lg-6">
                 <div class="card"> <br>
                     <center>
-                        <img src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap" 
+                        <img 
+                        src="{{(!empty($adminData->profile_image)) ? 
+                        url('upload/admin_images/'.$adminData->profile_image)
+                        :url('upload/no_image.jpg')}}" 
+                        alt="Card image cap" 
                         class="rounded-circle avatar-xl">
                     </center>
                     <div class="card-body">
